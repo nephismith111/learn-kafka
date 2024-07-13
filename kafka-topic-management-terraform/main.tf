4603f0c869438be2a62bead435fb035fb247d45b
Nephi Smith <nephi.smith.111@gmail.com>
2024-07-13 04:48:23 -0600

    I was reflecting on an in-memory deployment of kafka in a single data center and if there's a failure of all 3 nodes and the data is gone, manual action is required to restore the cluster. Right now, that would consist of a user being required to do a terraform apply. That's too much risk of increasing down time, especially for users who are not yet familiar with kafka and it's needs and behavior. A safer setup is to use local volumes and lock down the nodes on which the kafka containers can be deployed. I've added placement constraints for docker-server-1, 2 and 3. if these are true, then the kafka containers will remain on their respective hosts with their data volumes. This will make it so that no user interaction is required for auto recovery. We do need to deal with space considerations so the volumes do not grow without bound (should do that anyways, but cleanup processes will not auto fix this now).

--- kafka-topic-management-terraform/main.tf
+++ kafka-topic-management-terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "kafka" {
-    bootstrap_servers       = ["192.168.86.183:9092","172.19.0.2:9092"] 
+    bootstrap_servers       = ["192.168.86.183:9194","192.168.86.183:9294","192.168.86.183:9394"] 
     tls_enabled             = false
     # version           = "3.7.0"
     # bootstrap_servers = ["localhost:9092"] 
@@ -29,13 +29,13 @@ provider "kafka" {
 resource "kafka_topic" "topic1" {
     name                    =   "topic1"
     replication_factor      =   1
-    partitions              =   1
+    partitions              =   2
 }
 
-# resource "kafka_topic" "topic2" {
-#     name                    =   "topic2"
-#     replication_factor      =   var.replication_factor
-#     partitions              =   var.partitions
+resource "kafka_topic" "topic2" {
+    name                    =   "topic2"
+    replication_factor      =   2  #var.replication_factor
+    partitions              =   2  #var.partitions
     
-# }
+}
 
--- kafka-topic-management-terraform/variables.tf
+++ kafka-topic-management-terraform/variables.tf
@@ -1,21 +1,21 @@
-variable "environment" {
-    description = "Deployment environment: dev, prod, etc"
-    type = string
-}
+# variable "environment" {
+#     description = "Deployment environment: dev, prod, etc"
+#     type = string
+# }
 
-variable "replication_factor" {
-    description = "default replication factor for topics"
-    type = number
-}
+# variable "replication_factor" {
+#     description = "default replication factor for topics"
+#     type = number
+# }
 
-variable "partitions" {
-    description = "Default number of partitions per topics"
-    type = number
-}
+# variable "partitions" {
+#     description = "Default number of partitions per topics"
+#     type = number
+# }
 
-variable "kafka_bootstrap_servers" {
-    description = "Comma-separated lsit of kafka broker addressses"
-    type = list(string)
-}
+# variable "kafka_bootstrap_servers" {
+#     description = "Comma-separated lsit of kafka broker addressses"
+#     type = list(string)
+# }
 
 
